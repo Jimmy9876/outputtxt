abecf5d929abeb86cc29a2df7ce4052394e4e90d
Jimmy <jiwu@jideMacBook-Pro.lan>
2018-05-14 20:16:31 +0800

    new .ttf

---
+++ README.md
@@ -0,0 +1,21 @@
+# Windows须知
+## 1.安装环境
+下载Windows最简洁的`phpstudy`环境
+具体做法很简单，不一一说明了，详情请看这里的博客
+https://www.oschina.net/p/phpstudy
+## 2.目录说明
+本代码目录说明
+![](https://ws4.sinaimg.cn/large/006tKfTcgy1frb4ljs2wjj30ni0f6dgy.jpg)
+第一个是字体文件，第二个是说明文件，第三个是写入的txt文件，第四个是项目网页代码，第五个是背景图，缺一不可。
+## 3.代码运行
+安装好第一步的环境之后，用phpstudy打开output.php即可。
+浏览器打开后如下:
+![](https://ws2.sinaimg.cn/large/006tKfTcgy1frb4qbwlwqj31kw0w6duf.jpg)
+文本框输入文字，然后回车
+![](https://ws3.sinaimg.cn/large/006tKfTcgy1frb4q4nqraj31kw0w8wrx.jpg)
+OK，输入的文字将保存在a.txt
+![](https://ws1.sinaimg.cn/large/006tKfTcgy1frb4q579nyj30xs0i6q3i.jpg)
+
+**ps:多次输入自动换行保存**
+
+
--- output.php
+++ output.php
@@ -23,8 +23,12 @@ if($_POST['submit']){
     <meta charset="UTF-8">
     <title>textoutput</title>
     <style>
+        @font-face {
+            font-family: HYCCY;
+            src: url('HYChaoCuYuanJ.ttf');
+        }
         .div-height{border:1px solid #F00; width:1724px; height:954px; background: url(WechatIMG42.png)}
-        .inputtxt{margin-top:375px; margin-left: 140px; width: 1280px; height: 120px; border-radius: 10px; font-size: 80px; color:greenyellow }
+        .inputtxt{margin-top:375px; margin-left: 140px; width: 1280px; height: 120px; border-radius: 10px; font-size: 80px; color:rgb(0,255,0); font-family: HYCCY }
     </style>
 </head>
 <body>
